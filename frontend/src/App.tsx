80456a42ed206458f6d5263051b5daac8c8f98ac
liusheng.liu <liusheng.liu@cashfree.cn>
2026-03-08 14:59:36 +0800

    Fix: Use HashRouter for GitHub Pages SPA support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Category from './pages/Category';
@@ -9,7 +9,7 @@ import Admin from './pages/Admin';
 
 function App() {
   return (
-    <BrowserRouter basename="/alchemist">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -20,7 +20,7 @@ function App() {
           <Route path="admin" element={<Admin />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
